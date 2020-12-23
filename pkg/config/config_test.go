d2e8fc3bb61de65f617fecf0b6571a125d668177
Enrico Stahn <enrico.stahn@gmail.com>
2020-12-23 23:55:40 +1100

    test: config tests

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -9,6 +9,48 @@ import (
 
 // TestConfigParses validates if yaml annotation do not overlap
 func TestConfigParses(t *testing.T) {
-	cfg := Config{}
-	assert.NoError(t, yaml.Unmarshal([]byte(""), &cfg))
+	tests := []struct {
+		name   string
+		cfg    string
+		expCfg Config
+		expErr bool
+	}{
+		{
+			name:   "should render empty config",
+			cfg:    "",
+			expCfg: Config{},
+		},
+		{
+			name:   "should render multiple filters",
+			cfg:    `
+source:
+  filters:
+    - jmespath: "obj.metadata.namespace == 'kube-system'"
+    - jmespath: "obj.metadata.namespace != 'playground'"
+`,
+			expCfg: Config{
+				Source: Source{
+					Filters: []JMESPathFilter{
+						{JMESPath: "obj.metadata.namespace == 'kube-system'"},
+						{JMESPath: "obj.metadata.namespace != 'playground'"},
+					},
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			assert := assert.New(t)
+
+			gotCfg := Config{}
+			err := yaml.Unmarshal([]byte(test.cfg), &gotCfg)
+
+			if test.expErr {
+				assert.Error(err)
+			} else if assert.NoError(err) {
+				assert.Equal(test.expCfg, gotCfg)
+			}
+		})
+	}
 }
