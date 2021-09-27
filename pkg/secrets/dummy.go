0601372febf13e2cf15ce1ef4fe4f5fb0e87dcd4
Enrico Stahn <enrico.stahn@gmail.com>
2021-09-28 01:03:51 +1000

    test: test for dummy

--- pkg/secrets/dummy.go
+++ pkg/secrets/dummy.go
@@ -6,10 +6,12 @@ import v1 "k8s.io/api/core/v1"
 type DummyImagePullSecretsProvider struct {
 }
 
+// NewDummyImagePullSecretsProvider initialises a dummy image pull secrets provider
 func NewDummyImagePullSecretsProvider() ImagePullSecretsProvider {
 	return &DummyImagePullSecretsProvider{}
 }
 
+// GetImagePullSecrets returns an empty ImagePullSecretsResult
 func (p *DummyImagePullSecretsProvider) GetImagePullSecrets(pod *v1.Pod) (*ImagePullSecretsResult, error) {
 	return NewImagePullSecretsResult(), nil
 }
