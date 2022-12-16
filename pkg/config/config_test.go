9849df2e6f706ebd48c629b3bb1bf6ddb91faf32
Rémi BUISSON <remi-buisson@orange.fr>
2022-12-16 10:42:36 +0100

    feat: add custom tags to created ECR repositories (#191)
    This PR adds the ability to configure custom tags for created
    repositories.
    
    Co-authored-by: Enrico Stahn <enrico.stahn@gmail.com>

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -37,6 +37,44 @@ source:
 				},
 			},
 		},
+		{
+			name: "should render tags config",
+			cfg: `
+target:
+  type: aws
+  aws:
+    accountId: 123456789
+    region: ap-southeast-2
+    role: arn:aws:iam::123456789012:role/roleName
+    ecrOptions:
+      tags:
+        - key: CreatedBy
+          value: k8s-image-swapper
+        - key: A
+          value: B
+`,
+			expCfg: Config{
+				Target: Target{
+					AWS: AWS{
+						AccountID: "123456789",
+						Region:    "ap-southeast-2",
+						Role:      "arn:aws:iam::123456789012:role/roleName",
+						ECROptions: ECROptions{
+							Tags: []Tag{
+								{
+									Key:   "CreatedBy",
+									Value: "k8s-image-swapper",
+								},
+								{
+									Key:   "A",
+									Value: "B",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
